20daa52230ff06e72e0929c25b70148a401b5dc0
Federico Fiorio <fioriofederico99@gmail.com>
2025-10-04 16:46:39 +0200

    file path bugfix

--- satellite_ingestion/src/ingestion.py
+++ satellite_ingestion/src/ingestion.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Replace with your file path
-file_path = r"historical_traffic/history_traffic.csv"
+file_path = r"satellite_ingestion/historical_traffic/history_traffic.csv"
 
 # Load Excel file into DataFrame
 df = pd.read_csv(file_path, sep=';')
